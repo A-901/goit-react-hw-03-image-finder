b27f72a24a942514bfe9451fe3b664b502aadd28
Alexandr <e7000@ukr.net>
2020-06-21 15:10:50 +0300

    Updates

---
+++ precache-manifest.58d98ad28062510e5e09fb67c3355ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdff40dcf79dc6d90ee303302fbc0df",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "4964b603f2e912632506",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.849843cb.chunk.css"
+  },
+  {
+    "revision": "9101b8cc0ae2b853da43",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.91ea9d82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.91ea9d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4964b603f2e912632506",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c0a44c53.chunk.js"
+  },
+  {
+    "revision": "6bade45949e3f3efe12a",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.c201c8e6.js"
+  }
+]);
